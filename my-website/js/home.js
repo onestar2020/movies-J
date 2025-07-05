85e19da4467e31e77ab1a19081c8b529150ece4d
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2025-07-05 16:07:56 +0800

    my-website/js/home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -2,6 +2,15 @@ const API_KEY = '22d74813ded3fecbe3ef632b4814ae3a';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const IMG_URL = 'https://image.tmdb.org/t/p/original';
+let currentMoviePage = 1;
+let currentTVPage = 1;
+let currentAnimePage = 1;
+const moviesPerPage = 15;
+
+let movieItems = [];
+let tvItems = [];
+let animeItems = [];
+
 let currentItem;
 let currentUpload = null;
 let bannerItems = [];

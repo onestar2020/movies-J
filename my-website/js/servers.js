0cc01fd82cf5e8c09b504d8521c2ce5a34cbc3e3
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2025-07-07 13:15:05 +0800

    my-website/js/servers.js

--- my-website/js/servers.js
+++ my-website/js/servers.js
@@ -1,16 +1,16 @@
-const SERVER_LIST = [
-  { name: "Vidsrc.me", url: "vidsrc.me" },
-  { name: "MegaCloud.tv", url: "megacloud.tv" },
-  { name: "FzMovies.xyz", url: "fzmovies.xyz" },
-  { name: "Vidsrc.cc", url: "vidsrc.cc" },
-  { name: "Player.Videasy.net", url: "player.videasy.net" },
-  { name: "MultiEmbed.mov", url: "multiembed.mov" },
-  { name: "2Embed.to", url: "2embed.to" },
-  { name: "Zembed.net", url: "zembed.net" },
-  { name: "CurtStream", url: "curtstream.com" },
-  { name: "VidSrc Pro", url: "vidsrc.pro" },
-  { name: "AutoEmbed.to", url: "autoembed.to" },
-  { name: "2Embed.cc", url: "2embed.cc" },
-  { name: "DopeBox.to", url: "dopebox.to" },
-  { name: "SFlix.to", url: "sflix.to" }
+export const SERVER_LIST = [
+  "megacloud.tv",
+  "fzmovies.xyz",
+  "vidsrc.cc",
+  "vidsrc.me",
+  "player.videasy.net",
+  "multiembed.mov",
+  "2embed.to",
+  "zembed.net",
+  "curtstream.com",
+  "vidsrc.pro",
+  "autoembed.to",
+  "2embed.cc",
+  "dopebox.to",
+  "sflix.to"
 ];

b2d6217c8384eef05e18ac340722f2471d43c24f
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2025-07-14 12:15:46 +0800

    my-website/js/servers.js my-website/js/embed.js

--- my-website/js/embed.js
+++ my-website/js/embed.js
@@ -10,46 +10,9 @@ function generateEmbedURL(server, item, season = 1, episode = 1) {
         ? `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}`
         : `https://vidsrc.cc/v2/embed/movie/${id}`;
 
-    // 🟡 Partially working or under test
-    case "multiembed.mov":
-      return isTV
-        ? `https://multiembed.mov/tv/${id}/${season}/${episode}`
-        : `https://multiembed.mov/movie/${id}`;
-
-    case "2embed.to":
-      return isTV
-        ? `https://www.2embed.to/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`
-        : `https://www.2embed.to/embed/tmdb/movie?id=${id}`;
-
-    case "autoembed.to":
-      return isTV
-        ? `https://autoembed.to/tv/tmdb/${id}/${season}-${episode}`
-        : `https://autoembed.to/movie/tmdb/${id}`;
-
-    case "megacloud.tv":
-      return isTV
-        ? `https://megacloud.tv/embed?t=tv&id=${id}&s=${season}&e=${episode}`
-        : `https://megacloud.tv/embed?t=movie&id=${id}`;
-
-    case "dopebox.to":
-      return isTV
-        ? `https://dopebox.to/embedtv/${id}/${season}/${episode}`
-        : `https://dopebox.to/embed/${id}`;
-
-    case "sflix.to":
-      return isTV
-        ? `https://sflix.to/embed/tv/${id}/${season}/${episode}`
-        : `https://sflix.to/embed/movie/${id}`;
-
-    // ⚠️ Unverified or unstable
-    case "fzmovies.xyz":
-      return isTV
-        ? `https://fzmovies.xyz/tv/${id}/${season}/${episode}`
-        : `https://fzmovies.xyz/movie/${id}`;
-
     case "vidsrc.me":
       return isTV
-        ? `https://vidsrc.me/embed/tv?tmdb=${id}&season=${season}&episode=${episode}`
+        ? `https://vidsrc.net/embed/tv/?tmdb=${id}&season=${season}&episode=${episode}`
         : `https://vidsrc.net/embed/movie/?tmdb=${id}`;
 
     case "player.videasy.net":
@@ -57,26 +20,14 @@ function generateEmbedURL(server, item, season = 1, episode = 1) {
         ? `https://player.videasy.net/tv/${id}/${season}/${episode}`
         : `https://player.videasy.net/movie/${id}`;
 
-    case "zembed.net":
-      return `https://zembed.net/v/${id}`;
-
-    case "curtstream.com":
+    case "player.autoembed.cc":
       return isTV
-        ? `https://www.curtstream.com/embed/tv/${id}/${season}/${episode}`
-        : `https://www.curtstream.com/embed/movie/${id}`;
-
-    case "vidsrc.pro":
-      return isTV
-        ? `https://vidsrc.pro/embed/tv/${id}/${season}/${episode}`
-        : `https://vidsrc.pro/embed/movie/${id}`;
-
-    case "2embed.cc":
-      return isTV
-        ? `https://2embed.cc/embedtv/${id}/${season}-${episode}`
-        : `https://2embed.cc/embed/${id}`;
+        ? `https://player.autoembed.cc/embed/tv/${id}/${season}/${episode}`
+        : `https://player.autoembed.cc/embed/movie/${id}`;
 
     default:
       return "";
   }
 }
+
 window.generateEmbedURL = generateEmbedURL;
--- my-website/js/servers.js
+++ my-website/js/servers.js
@@ -1,22 +1,9 @@
 const SERVER_LIST = [
   // ✅ Confirmed working
   "vidsrc.cc",
-
-  // 🟡 Partially working or under observation
-  "multiembed.mov",
-  "2embed.to",
-  "autoembed.to",
-  "megacloud.tv",
-  "dopebox.to",
-  "sflix.to",
-
-  // ⚠️ Unverified or unstable
-  "fzmovies.xyz",
   "vidsrc.me",
   "player.videasy.net",
-  "zembed.net",
-  "curtstream.com",
-  "vidsrc.pro",
-  "2embed.cc"
+  "player.autoembed.cc"
 ];
+
 window.SERVER_LIST = SERVER_LIST;

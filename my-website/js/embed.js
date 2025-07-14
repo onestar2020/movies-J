de280563ef094cd584a9dbcd62ff5a35380c8cd3
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2025-07-14 12:36:48 +0800

    my-website/js/embed.js

--- my-website/js/embed.js
+++ my-website/js/embed.js
@@ -4,22 +4,25 @@ function generateEmbedURL(server, item, season = 1, episode = 1) {
   const isTV = type === "tv";
 
   switch (server) {
-    // ✅ Confirmed working
+    // ✅ vidsrc.cc
     case "vidsrc.cc":
       return isTV
         ? `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}`
         : `https://vidsrc.cc/v2/embed/movie/${id}`;
 
+    // ✅ vidsrc.me (using vidsrc.net URLs)
     case "vidsrc.me":
       return isTV
         ? `https://vidsrc.net/embed/tv/?tmdb=${id}&season=${season}&episode=${episode}`
         : `https://vidsrc.net/embed/movie/?tmdb=${id}`;
 
+    // ✅ player.videasy.net
     case "player.videasy.net":
       return isTV
         ? `https://player.videasy.net/tv/${id}/${season}/${episode}`
         : `https://player.videasy.net/movie/${id}`;
 
+    // ✅ player.autoembed.cc
     case "player.autoembed.cc":
       return isTV
         ? `https://player.autoembed.cc/embed/tv/${id}/${season}/${episode}`

610c53e4a331abd5dfad60d31559d72ea233fa7e
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2025-07-18 10:16:16 +0800

    my-website/index.html my-website/sw.js

--- my-website/sw.js
+++ my-website/sw.js
@@ -1,5 +1,5 @@
 // ✅ sw.js - Service Worker for Movies-J
-const CACHE_VERSION = 'v1.1.4'; // 🔁 Increment this to trigger updates
+const CACHE_VERSION = 'v1.1.6'; // 🔁 Increment this to trigger updates
 const CACHE_NAME = `movie-cache-${CACHE_VERSION}`;
 
 const urlsToCache = [

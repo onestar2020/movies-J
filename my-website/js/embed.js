49e79b9de752a399b0c1d64fd6ebcc3353fb5969
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2026-04-07 10:58:02 +0800

    Add files via upload

--- my-website/js/embed.js
+++ my-website/js/embed.js
@@ -1,4 +1,4 @@
-// ✅ IN-UPDATE PARA SA 2EMBED.TO
+// ✅ js/embed.js - UPDATED API LINKS
 
 function generateEmbedURL(server, item, season = 1, episode = 1) {
   const id = item.id;
@@ -6,34 +6,24 @@ function generateEmbedURL(server, item, season = 1, episode = 1) {
   const isTV = item.media_type === "tv" || item.first_air_date; 
 
   switch (server) {
-    // Bagong case para sa 2embed.to
-    case "2embed.to":
-      return isTV
-        ? `https://www.2embed.to/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`
-        : `https://www.2embed.to/embed/tmdb/movie?id=${id}`;
-
-    // Mga dati mong server
-    case "vidsrc.cc":
-      return isTV
-        ? `https://vidsrc.cc/v2/embed/tv/${id}/${season}/${episode}`
-        : `https://vidsrc.cc/v2/embed/movie/${id}`;
-
-    case "vidsrc.me":
-      return isTV
-        ? `https://vidsrc.net/embed/tv/?tmdb=${id}&season=${season}&episode=${episode}`
-        : `https://vidsrc.net/embed/movie/?tmdb=${id}`;
-
-    case "player.videasy.net":
-      return isTV
-        ? `https://player.videasy.net/tv/${id}/${season}/${episode}`
-        : `https://player.videasy.net/movie/${id}`;
-
-    case "player.autoembed.cc":
+    case "autoembed":
       return isTV
         ? `https://player.autoembed.cc/embed/tv/${id}/${season}/${episode}`
         : `https://player.autoembed.cc/embed/movie/${id}`;
 
+    case "vidsrc.net":
+      return isTV
+        ? `https://vidsrc.net/embed/tv?tmdb=${id}&season=${season}&episode=${episode}`
+        : `https://vidsrc.net/embed/movie?tmdb=${id}`;
+
+    case "superembed":
+      return isTV
+        ? `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1&s=${season}&e=${episode}`
+        : `https://multiembed.mov/directstream.php?video_id=${id}&tmdb=1`;
+
     default:
-      return "";
+      return isTV
+        ? `https://player.autoembed.cc/embed/tv/${id}/${season}/${episode}`
+        : `https://player.autoembed.cc/embed/movie/${id}`;
   }
 }
--- my-website/js/servers.js
+++ my-website/js/servers.js
@@ -1,7 +1,9 @@
+// ✅ js/servers.js - UPDATED FAST SERVERS
+
 const SERVER_LIST = [
-  { displayName: "Server 1 (Best)", realName: "vidsrc.me" },
-  { displayName: "Server 2", realName: "vidsrc.cc" },
-  { displayName: "Server 3 (Alternative)", realName: "2embed.to" }
+  { displayName: "Server 1 (Fastest)", realName: "autoembed" },
+  { displayName: "Server 2 (Stable)", realName: "vidsrc.net" },
+  { displayName: "Server 3 (Backup)", realName: "superembed" }
 ];
 
 window.SERVER_LIST = SERVER_LIST;

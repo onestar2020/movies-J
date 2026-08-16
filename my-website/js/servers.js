fd7077ba00e7b0c31ba6ad3b94bda145f8d29a87
onestar2020 <65755336+onestar2020@users.noreply.github.com>
2026-08-16 14:58:49 +0800

    Update stream servers and player

--- my-website/js/servers.js
+++ my-website/js/servers.js
@@ -1,68 +1,72 @@
 /**
  * Movies-J - Stream Servers Configuration
- * Clean list without duplicates.
+ * Active: Server 1 (Vidstorm), Server 2 (CineSRC), Server 3 (2Embed), Server 6 (ZXCStream)
+ * Hidden: Server 4 (Nova), Server 5 (VidLink), Server 7 (YaStream)
  */
 
 const STREAM_SERVERS = {
-  // Server 1 - Vidstorm (Requires IMDb ID)
+  // Server 1 - Vidstorm
   vidstorm: {
     id: "vidstorm",
-    name: "Server 1 (Vidstorm)",
+    name: "Server 1",
     type: "iframe",
+    enabled: true,
     movie: (imdbId) => `https://vidstorm.ru/movie/${imdbId}?autoplay=true&theme=7c5cff&download=false&lang=en`,
     tv: (imdbId, s = 1, e = 1) => `https://vidstorm.ru/tv/${imdbId}/${s}/${e}?autoplay=true&theme=7c5cff&download=false&lang=en`
   },
 
-  // Server 2 - CineSRC (TMDb ID)
+  // Server 2 - CineSRC
   cinesrc: {
     id: "cinesrc",
-    name: "Server 2 (CineSRC)",
+    name: "Server 2",
     type: "iframe",
+    enabled: true,
     movie: (tmdbId) => `https://cinesrc.st/embed/movie/${tmdbId}`,
     tv: (tmdbId, s = 1, e = 1) => `https://cinesrc.st/embed/tv/${tmdbId}?s=${s}&e=${e}`
   },
 
-  // Server 3 - 2Embed (TMDb ID)
+  // Server 3 - 2Embed
   twoembed: {
     id: "twoembed",
-    name: "Server 3 (2Embed)",
+    name: "Server 3",
     type: "iframe",
+    enabled: true,
     movie: (tmdbId) => `https://www.2embed.skin/embed/${tmdbId}`,
     tv: (tmdbId, s = 1, e = 1) => `https://www.2embed.skin/embedtv/${tmdbId}&s=${s}&e=${e}`
   },
 
-  // Server 4 - Nova / CloudOrchestra (TMDb ID)
-  cloudorchestra: {
-    id: "cloudorchestra",
-    name: "Server 4 (Nova)",
-    type: "iframe",
-    movie: (tmdbId) => `https://cloudorchestranova.com/embed/movie/${tmdbId}`,
-    tv: (tmdbId, s = 1, e = 1) => `https://cloudorchestranova.com/embed/tv/${tmdbId}/${s}/${e}`
-  },
-
-  // Server 5 - VidLink (TMDb ID)
-  vidlink: {
-    id: "vidlink",
-    name: "Server 5 (VidLink)",
-    type: "iframe",
-    movie: (tmdbId) => `https://vidlink.pro/movie/${tmdbId}?autoplay=true`,
-    tv: (tmdbId, s = 1, e = 1) => `https://vidlink.pro/tv/${tmdbId}/${s}/${e}?autoplay=true`
-  },
-
-  // Server 6 - ZXCStream (TMDb ID)
+  // Server 4 - ZXCStream (Dating Server 6)
   zxcstream: {
     id: "zxcstream",
-    name: "Server 6 (ZXCStream)",
+    name: "Server 4",
     type: "iframe",
+    enabled: true,
     movie: (tmdbId) => `https://player.zxcstream.xyz/player/movie/${tmdbId}`,
     tv: (tmdbId, s = 1, e = 1) => `https://player.zxcstream.xyz/player/tv/${tmdbId}/${s}/${e}`
   },
 
-  // Server 7 - Asian / Global Scraper (KKPhim, KissKH, OneTouchTV, etc.)
+  // Hidden / Disabled Servers (Naka-hide muna)
+  cloudorchestra: {
+    id: "cloudorchestra",
+    name: "Server (Nova)",
+    type: "iframe",
+    enabled: false,
+    movie: (tmdbId) => `https://cloudorchestranova.com/embed/movie/${tmdbId}`,
+    tv: (tmdbId, s = 1, e = 1) => `https://cloudorchestranova.com/embed/tv/${tmdbId}/${s}/${e}`
+  },
+  vidlink: {
+    id: "vidlink",
+    name: "Server (VidLink)",
+    type: "iframe",
+    enabled: false,
+    movie: (tmdbId) => `https://vidlink.pro/movie/${tmdbId}?autoplay=true`,
+    tv: (tmdbId, s = 1, e = 1) => `https://vidlink.pro/tv/${tmdbId}/${s}/${e}?autoplay=true`
+  },
   yastream: {
     id: "yastream",
-    name: "Server 7 (KKPhim / KissKH)",
+    name: "Server (KKPhim)",
     type: "api_addon",
+    enabled: false,
     endpoint: "https://yastream.tamthai.de/eyJjYXRhbG9ncyI6WyJpZHJhbWEuc2VyaWVzLmlEcmFtYSIsImlkcmFtYS5zZXJpZXMuU2VhcmNoIiwia2lzc2toLnNlcmllcy5DaGluZXNlIiwia2lzc2toLnNlcmllcy5Lb3JlYW4iLCJraXNza2guc2VyaWVzLkphcGFuZXNlIiwia2lzc2toLnNlcmllcy5Ib25na29uZyIsImtpc3NraC5zZXJpZXMuVGhhaSIsImtpc3NraC5zZXJpZXMuVVMiLCJraXNza2guc2VyaWVzLlRhaXdhbmVzZSIsImtpc3NraC5zZXJpZXMuUGhpbGlwcGluZSIsImtpc3NraC5zZXJpZXMuU2VhcmNoIiwia2lzc2toLm1vdmllLkNoaW5lc2UiLCJraXNza2gubW92aWUuS29yZWFuIiwia2lzc2toLm1vdmllLkphcGFuZXNlIiwia2lzc2toLm1vdmllLkhvbmdrb25nIiwia2lzc2toLm1vdmllLlRoYWkiLCJraXNza2gubW92aWUuVVMiLCJraXNza2gubW92aWUuVGFpd2FuZXNlIiwia2lzc2toLm1vdmllLlBoaWxpcHBpbmUiLCJraXNza2gubW92aWUuU2VhcmNoIiwib25ldG91Y2h0di5zZXJpZXMuUG9wdWxhciIsIm9uZXRvdWNodHYuc2VyaWVzLkNoaW5lc2UiLCJvbmV0b3VjaHR2LnNlcmllcy5Lb3JlYW4iLCJvbmV0b3VjaHR2LnNlcmllcy5UaGFpIiwib25ldG91Y2h0di5zZXJpZXMuU2VhcmNoIiwib25ldG91Y2h0di5tb3ZpZS5Qb3B1bGFyIiwib25ldG91Y2h0di5tb3ZpZS5DaGluZXNlIiwib25ldG91Y2h0di5tb3ZpZS5Lb3JlYW4iLCJvbmV0b3VjaHR2Lm1vdmllLlRoYWkiLCJvbmV0b3VjaHR2Lm1vdmllLlNlYXJjaCJdLCJjYXRhbG9nIjpbImtpc3NraCIsIm9uZXRvdWNodHYiLCJpZHJhbWEiXSwic3RyZWFtIjpbImtpc3NraCIsIm9uZXRvdWNodHYiLCJta3ZkcmFtYSIsImlkcmFtYSIsImtrcGhpbSIsIm9waGltIl0sIm5zZnciOmZhbHNlLCJpbmZvIjpmYWxzZSwicG9zdGVyIjoicnBkYiIsIm1mcFVybCI6IiIsInRiS2V5IjoiIiwibWZwUGFzcyI6IiIsImVtYWlsIjoiIiwiaXAiOiIifQ",
     getStreamUrl: (imdbId, type = "movie", s = 1, e = 1) => {
       const idStr = type === "movie" ? imdbId : `${imdbId}:${s}:${e}`;
@@ -71,7 +75,6 @@ const STREAM_SERVERS = {
   }
 };
 
-// Helper function para mag-generate ng embed URL para sa iframe
 function getEmbedUrl(serverKey, mediaData, type = "movie", s = 1, e = 1) {
   const server = STREAM_SERVERS[serverKey];
   if (!server) return "";
